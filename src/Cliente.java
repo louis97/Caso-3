0d17d8a8ec3567f2fa1b89f64d273cb6ce62ea92
JULIAN ESTEBAN OSORIO RAMIREZ <je.osorio@sis.virtual.uniandes.edu.co>
2019-10-24 15:17:42 -0500

    terminado, creo

--- src/Cliente.java
+++ src/Cliente.java
@@ -19,161 +19,227 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.xml.bind.DatatypeConverter;
 
+import org.bouncycastle.crypto.digests.SHA1Digest;
+import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.tls.SSL3Mac;
 import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class Cliente {
-	
+
 	private final static int PORT = 1245;
 	private final static String HOST = "localhost";
-	
+
 	private final static String ALGORITMOS = "ALGORITMOS";
 	private final static String PADDING = "AES/ECB/PKCS5Padding";
-	
-	private enum  ALG
-	{
-	    AES("AES"),
-	    BLOWFISH("BLOWFISH"), 
-	    RSA("RSA"),
-	    HMACSHA1("HMACSHA1"),
-	    HMACSHA256("HMACSHA256"),
-	    HMACSHA384("HMACSHA384"), 
-	    HMACSHA512("HMACSHA512");
+
+	private enum ALG {
+		AES("AES"), BLOWFISH("BLOWFISH"), RSA("RSA"), HMACSHA1("HMACSHA1"), HMACSHA256("HMACSHA256"), HMACSHA384(
+				"HMACSHA384"), HMACSHA512("HMACSHA512");
 		private final String S;
-		private ALG(final String S){
-			this.S=S;
+
+		private ALG(final String S) {
+			this.S = S;
 		}
-		public String getS(){
+
+		public String getS() {
 			return S;
 		}
 	}
-	
+
 	private static String mensajeServidor;
-//	private static ServerSocket ss;
+	// private static ServerSocket ss;
 	private static Socket cs;
 	private static PrintWriter writer;
 	private static BufferedReader reader;
 	private static PublicKey llavePublicaServidor;
-	
-	public static void extraerPKCD(String fromServer) throws CertificateException{
-		//Se parsea el formato hexadecimal del certificado del servidor a un formato de certificado X509
+
+	public static void extraerPKCD(String fromServer) throws CertificateException {
+		// Se parsea el formato hexadecimal del certificado del servidor a un
+		// formato de certificado X509
 		byte[] certificadoBytes = DatatypeConverter.parseBase64Binary(fromServer);
 		CertificateFactory creador = CertificateFactory.getInstance("X.509");
-		InputStream in =  new ByteArrayInputStream(certificadoBytes);
-		X509Certificate certificadoServidor = (X509Certificate)creador.generateCertificate(in);
-		
-		//Se obtiene la llave publica del servidor a partir del certificado
+		InputStream in = new ByteArrayInputStream(certificadoBytes);
+		X509Certificate certificadoServidor = (X509Certificate) creador.generateCertificate(in);
+
+		// Se obtiene la llave publica del servidor a partir del certificado
 		llavePublicaServidor = certificadoServidor.getPublicKey();
 	}
-	
-	public static byte[] cifrarA (Key llave, String algoritmo, String texto){
+
+	public static byte[] cifrarA(Key llave, String algoritmo, String texto) {
 		byte[] textoCifrado;
-		
-		try{
+
+		try {
 			Cipher cifrador = Cipher.getInstance(algoritmo);
 			byte[] textoClaro = texto.getBytes();
-			
+
 			cifrador.init(Cipher.ENCRYPT_MODE, llave);
 			textoCifrado = cifrador.doFinal(textoClaro);
-			
+
 			return textoCifrado;
-		}
-		catch(Exception e){
-			System.out.println("Excepcion: "+e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Excepcion: " + e.getMessage());
 			return null;
 		}
 	}
-	public static byte[] descifrarS (SecretKey llave, byte[] texto){
+
+	public static byte[] cifrarS(SecretKey llave, String algoritmo, String texto) {
+		byte[] textoCifrado;
+
+		try {
+			Cipher cifrador = Cipher.getInstance(PADDING);
+			byte[] textoClaro = texto.getBytes();
+
+			cifrador.init(Cipher.ENCRYPT_MODE, llave);
+			textoCifrado = cifrador.doFinal(textoClaro);
+
+			return textoCifrado;
+		} catch (Exception e) {
+			System.out.println("Excepcion: " + e.getMessage());
+			return null;
+		}
+	}
+
+	public static byte[] descifrarS(SecretKey llave, byte[] texto) {
 		byte[] textoClaro;
 		try {
-			Cipher cifrador= Cipher.getInstance(PADDING);
+			Cipher cifrador = Cipher.getInstance(PADDING);
 			cifrador.init(Cipher.DECRYPT_MODE, llave);
-			textoClaro= cifrador.doFinal(texto);
+			textoClaro = cifrador.doFinal(texto);
 
 		} catch (Exception e) {
-			System.out.println("Exception: "+ e.getMessage());
+			System.out.println("Exception: " + e.getMessage());
 			return null;
 		}
 		return textoClaro;
 	}
-	
-	
-	
-   public static byte[] verficarLongCadena(String cadena){ 
-		String res = cadena;
-		while(res.length()%4!=0){
-			res="0"+res;
-			System.out.println(res);
-			
+
+	public static byte[] descifrarA(Key llave, String algoritmo, byte[] texto) {
+		byte[] textoClaro;
+
+		try {
+
+			Cipher cifrador = Cipher.getInstance(algoritmo);
+			cifrador.init(Cipher.DECRYPT_MODE, llave);
+			textoClaro = cifrador.doFinal(texto);
+
+		} catch (Exception e) {
+
+			System.out.println("Excepcion: " + e.getMessage());
+			return null;
+
 		}
-		System.out.println("length: "+res.length());
-	return DatatypeConverter.parseBase64Binary(res);
-}
-	
-	
-   public static byte[] verficarLongCadena16(String cadena){ 
+
+		return textoClaro;
+	}
+
+	public static byte[] verficarLongCadena(String cadena) {
 		String res = cadena;
-		while(res.length()%16!=0){
-			res="0"+res;
+		while (res.length() % 4 != 0) {
+			res = "0" + res;
 			System.out.println(res);
-			
+
 		}
-		System.out.println("length: "+res.length());
-	return DatatypeConverter.parseBase64Binary(res);
-}
+		System.out.println("length: " + res.length());
+		return DatatypeConverter.parseBase64Binary(res);
+	}
+
+	public static byte[] verficarLongCadena16(String cadena) {
+		String res = cadena;
+		while (res.length() % 16 != 0) {
+			System.out.println(res);
+		}
+		return DatatypeConverter.parseBase64Binary(res);
+	}
+
 	public static void main(String[] args) {
 		try {
+			boolean ok = true;
 			cs = new Socket(HOST, PORT);
 			writer = new PrintWriter(cs.getOutputStream(), true);
 			reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-			
+
 			writer.println("HOLA");
 			System.out.println(reader.readLine());
-			
-			writer.println(ALGORITMOS+":"+ALG.AES.getS()+":"+ALG.RSA.getS()+":"+ALG.HMACSHA256.getS());
+
+			writer.println(ALGORITMOS + ":" + ALG.AES.getS() + ":" + ALG.RSA.getS() + ":" + ALG.HMACSHA256.getS());
 			System.out.println(reader.readLine());
-			
+
 			String cd = reader.readLine();
 			System.out.println(cd);
 			extraerPKCD(cd);
-			KeyGenerator keygen= KeyGenerator.getInstance("AES");		
-			SecretKey ks= keygen.generateKey();	
-	
-			System.out.println("Baina: "+DatatypeConverter.printBase64Binary(ks.getEncoded()));
+			KeyGenerator keygen = KeyGenerator.getInstance("AES");
+			SecretKey ks = keygen.generateKey();
+
+			System.out.println("Baina: " + DatatypeConverter.printBase64Binary(ks.getEncoded()));
 			String a = new String(ks.getEncoded());
 			byte[] ksCifrada = cifrarA(llavePublicaServidor, ALG.RSA.getS(), a);
-			String ksCifradaS= DatatypeConverter.printBase64Binary(ksCifrada);
-			
-			System.out.println("llave de sesion: "+ DatatypeConverter.printBase64Binary(ks.getEncoded()));
-			writer.println(ksCifradaS);
-			
-			writer.println("reto");
-			System.out.println("se envió: reto");
-			
-			cd = reader.readLine();
-			System.out.println("length cd: "+cd.length());
-			System.out.println("Reto cifrado: "+cd);
-			byte[] retoDesc = descifrarS(ks,verficarLongCadena(cd)); 
-			String a1=DatatypeConverter.printBase64Binary(retoDesc);
-			System.out.println("reto descifrado: "+a1);
-			
-//			System.out.println("se recibe cod encriptado: "+cd);
-//			
-//			byte[] prueba1=verficarLongCadena(cd);
-//			System.out.println("se verifica cadena : "+prueba1);
-//			
-//			byte[] prueba = descifrarS(sk, prueba1);
-//			System.out.println("se descifra cadena : "+prueba);
-//			
-//			String retoD = DatatypeConverter.printBase64Binary(prueba);
-//			
-//			System.out.println("se descifró reto: "+retoD);
-			
-		} catch (IOException | CertificateException | NoSuchAlgorithmException e) {
-		
-			e.printStackTrace();
-		} 
+			String ksCifradaS = DatatypeConverter.printBase64Binary(ksCifrada);
 
+			System.out.println("llave de sesion: " + DatatypeConverter.printBase64Binary(ks.getEncoded()));
+			writer.println(ksCifradaS);
+
+			String reto = "reto";
+			writer.println(reto);
+			System.out.println("se envió: reto");
+
+			cd = reader.readLine();
+			System.out.println("Reto cifrado: " + cd);
+			try {
+				byte[] retoDesc = descifrarS(ks, verficarLongCadena(cd));
+				String a1 = DatatypeConverter.printBase64Binary(retoDesc);
+				System.out.println("reto descifrado: " + a1);
+				if (!a1.equals(reto)) {
+					writer.println("ERROR");
+					ok = false;
+				} else {
+					writer.println("OK");
+				}
+			} catch (Exception e) {
+				writer.println("ERROR");
+				ok = false;
+			}
+			if (ok) {
+				String cc = "cc";
+				byte[] ccCifrado = cifrarS(ks, "", cc);
+				String ccCifradoS = DatatypeConverter.printBase64Binary(ccCifrado);
+				writer.println(ccCifradoS);
+
+				String clave = "clave";
+				byte[] claveCifrado = cifrarS(ks, "", clave);
+				String claveCifradoS = DatatypeConverter.printBase64Binary(claveCifrado);
+				writer.println(claveCifradoS);
+
+				String cifradoValor = reader.readLine();
+				byte[] descifradoValor = descifrarS(ks, verficarLongCadena(cifradoValor));
+
+				String hmacC = reader.readLine();
+				byte[] hmacB = descifrarA(llavePublicaServidor, ALG.RSA.getS(), verficarLongCadena(hmacC));
+				String hmacBS = DatatypeConverter.printBase64Binary(hmacB);
+
+				HMac hmac = new HMac(new SHA256Digest());;
+				hmac.init(new KeyParameter(ks.getEncoded()));
+				byte[] hashBytes = new byte[hmac.getMacSize()];
+				hmac.update(descifradoValor, 0, descifradoValor.length);
+				hmac.doFinal(hashBytes, 0);
+
+				String hmacR = DatatypeConverter.printBase64Binary(hashBytes);
+				System.out.println("hmac calculado: " + hmacR);
+
+				if (!(hmacR.equals(hmacBS))) {
+					writer.println("ERROR");
+					ok = false;
+				}
+				else{
+					writer.println("OK");
+				}
+			}
+
+		} catch (IOException | CertificateException | NoSuchAlgorithmException e) {
+
+			e.printStackTrace();
+		}
 
 	}
 }
